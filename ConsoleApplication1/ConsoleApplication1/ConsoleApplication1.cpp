cbe1f42bb1ab849bf56a38ba1cb6038b0752faef
nazmicanyx <132282133+nazmicanyx@users.noreply.github.com>
2023-05-04 17:26:53 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -20,9 +20,9 @@ void printFrequency(int arr[], int size) {
 }
 
 int main() {
-    int arr[] = { 1, 2, 3, 2, 1, 3, 4, 5, 4, 2 }; // verilen dizi
-    int size = sizeof(arr) / sizeof(arr[0]); // dizi boyutu hesaplanıyor
+    int arr[] = { 1, 2, 3, 2, 1, 3, 4, 5, 4, 2 }; 
+    int size = sizeof(arr) / sizeof(arr[0]); 
     printf("Dizi elemanları ve frekansları:\n");
-    printFrequency(arr, size); // fonksiyon çağrılıyor
+    printFrequency(arr, size); 
     return 0;
 }
